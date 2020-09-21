580de677105585551ce761239e19f4b0b39f101e
Tend <casablancaernesto@gmail.com>
2020-09-21 15:28:30 +0200

    fix: master_doc needed to be 'index'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,6 +40,9 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+#Set the default .rts file
+master_doc = 'index'
+
 
 # -- Options for HTML output -------------------------------------------------
 

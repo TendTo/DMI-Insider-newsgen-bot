10462d558fd2ac70fab2b502486ae1e8d3c17ceb
Tend <casablancaernesto@gmail.com>
2020-09-21 16:04:05 +0200

    fix: the settings.yaml.dist file is renamed before the documentation process

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,3 +55,10 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Initial setup
+def setup(app):
+    app.connect('config-inited', mv_settings)
+
+def mv_settings(app,config):
+    os.rename(f'{app.srcdir}/../../config/settings.yaml.dist', f'{app.srcdir}/../../config/settings.yaml')
